eeba353667b1aae342b8dbd5142c693f2f419cd7
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-15 14:51:04 -0300

    (build): ambiente de dev do StoreManager

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// não remova esse endpoint, é para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.use(express.json());
+
+app.use(error);
+
+// não remova essa exportação, é para o avaliador funcionar
+// você pode registrar suas rotas normalmente, como o exemplo acima
+// você deve usar o arquivo index.js para executar sua aplicação 
+module.exports = app;

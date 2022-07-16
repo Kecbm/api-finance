330608e06b960a20890cc04fd7989d33b6b5f4e7
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-16 17:48:22 -0300

    (feat): ao comprar a qtde ativo na tab é subtraida

--- models/investimentosModel.js
+++ models/investimentosModel.js
@@ -4,8 +4,13 @@ const comprar = async (body) => {
   const { codCliente, codAtivo, qtdeAtivo } = body;
 
   await connection.execute(
-    'INSERT INTO XPInvestimentos.comprar (codCliente, codAtivo, qtdeAtivo) VALUES (?, ?, ?);',
+    'INSERT INTO XPInvestimentos.comprar (codCliente, codAtivo, qtdeAtivo) VALUES (?, ?, ?)',
     [codCliente, codAtivo, qtdeAtivo],
+  );
+
+  await connection.execute(
+    `UPDATE XPInvestimentos.ativo SET qtdeAtivo = qtdeAtivo - ${qtdeAtivo} WHERE codAtivo = ?`,
+    [codAtivo],
   );
 
   return `Parabéns cliente ${codCliente}, você acabou de comprar ${qtdeAtivo} unidades da ação ${codAtivo}`;

a5224570b97dddb86aeece1209c5dc117edf682a
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-16 16:52:58 -0300

    (feat): validação da qtde de ativos na corretora

--- controllers/investimentosController.js
+++ controllers/investimentosController.js
@@ -3,6 +3,10 @@ const investimentosService = require('../services/investimentosService');
 const comprar = async (req, res) => {
     const novaCompra = await investimentosService.comprar(req.body);
 
+    if (!novaCompra) {
+      return res.status(404).json({ message: 'Desculpe, a corretora não possui essa quantidade do ativo' });
+    }
+
     return res.status(201).json(novaCompra);
 };
 
--- services/investimentosService.js
+++ services/investimentosService.js
@@ -1,6 +1,15 @@
 const investimentosModel = require('../models/investimentosModel');
+const ativoService = require('../services/ativoService');
 
 const comprar = async (body) => {
+  const { codAtivo, qtdeAtivo } = body;
+
+  const quantidadeDisponivel = await ativoService.quantity(codAtivo);
+
+  if (quantidadeDisponivel > qtdeAtivo) {
+    return;
+  }
+
   const novaCompra = await investimentosModel.comprar(body);
 
   return novaCompra;

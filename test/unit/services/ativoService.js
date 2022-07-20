9833cfe26f5e993fabe394d16f5f851072ba2749
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-20 15:20:27 -0300

    :heavy_check_mark: test: testes de ativoService

--- test/unit/services/ativoService.js
+++ test/unit/services/ativoService.js
@@ -25,7 +25,73 @@ const dataAtivos = [
   },
 ];
 
-describe('Testando o ativoModel', () => {
+const dataCarteira = [
+  {
+    codCliente: 1,
+    codAtivo: 1,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 1,
+    codAtivo: 2,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 1,
+    codAtivo: 3,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 2,
+    codAtivo: 1,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 2,
+    codAtivo: 2,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 2,
+    codAtivo: 3,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 3,
+    codAtivo: 1,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 3,
+    codAtivo: 2,
+    qtdeAtivo: 20
+  },
+  {
+    codCliente: 3,
+    codAtivo: 3,
+    qtdeAtivo: 20
+  },
+];
+
+const dataAssets = [
+  {
+    CodAtivo: 1,
+    Valor: 50,
+    QtdeAtivo: 100,
+  },
+  {
+    CodAtivo: 2,
+    Valor: 25,
+    QtdeAtivo: 100,
+  },
+  {
+    CodAtivo: 3,
+    Valor: 100,
+    QtdeAtivo: 100,
+  },
+];
+
+describe('Testando o ativoService', () => {
   describe('ativoService.quantity', () => {
     before(() => {
       const stubModelGetById = sinon.stub(ativoModel, 'quantity');
@@ -113,4 +179,181 @@ describe('Testando o ativoModel', () => {
       expect(response).to.be.empty;
     });
   });
+
+  describe('ativoService.carteira', () => {
+    before(() => {
+      const stubModelGetById = sinon.stub(ativoModel, 'carteira');
+  
+      stubModelGetById.onCall(0).resolves([dataCarteira[0]]);
+      stubModelGetById.onCall(1).resolves([dataCarteira[0]]);
+      stubModelGetById.onCall(2).resolves([dataCarteira[0]]);
+      stubModelGetById.onCall(3).resolves([dataCarteira[1]]);
+      stubModelGetById.onCall(4).resolves([dataCarteira[1]]);
+      stubModelGetById.onCall(5).resolves([dataCarteira[1]]);
+      stubModelGetById.onCall(6).resolves([dataCarteira[2]]);
+      stubModelGetById.onCall(7).resolves([dataCarteira[2]]);
+      stubModelGetById.onCall(8).resolves([dataCarteira[2]]);
+      stubModelGetById.onCall(9).resolves([]);
+      stubModelGetById.onCall(10).resolves([]);
+    });
+  
+    after(() => {
+      ativoModel.carteira.restore();
+    });
+  
+    it('Testa se o retorno para o id 1 é um array', async () => {
+      const response = await ativoService.carteira(1);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 1 não está vazio', async () => {
+      const response = await ativoService.carteira(1);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna a quantidade de ativo da carteira do cliente de id 1', async () => {
+      const [response] = await ativoService.carteira(1);
+      
+      expect(response).to.have.property('qtdeAtivo');
+    });
+
+    it('Testa se o retorno para o id 2 é um array', async () => {
+      const response = await ativoService.carteira(2);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 2 não está vazio', async () => {
+      const response = await ativoService.carteira(2);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna a quantidade de ativo da carteira do cliente de id 2', async () => {
+      const [response] = await ativoService.carteira(2);
+      
+      expect(response).to.have.property('qtdeAtivo');
+    });
+
+    it('Testa se o retorno para o id 3 é um array', async () => {
+      const response = await ativoService.carteira(3);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 3 não está vazio', async () => {
+      const response = await ativoService.carteira(3);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna a quantidade de ativo da carteira do cliente de id 3', async () => {
+      const [response] = await ativoService.carteira(3);
+      
+      expect(response).to.have.property('qtdeAtivo');
+    });
+
+    it('Testa se o retorno para um id inválido é um array', async () => {
+      const response = await ativoService.carteira(4);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para um id inválido está vazio', async () => {
+      const response = await ativoService.carteira(4);
+  
+      expect(response).to.be.empty;
+    });
+  });
+
+  describe('ativoService.getByAssets', () => {
+    before(() => {
+      const stubModelGetById = sinon.stub(ativoModel, 'getByAssets');
+  
+      stubModelGetById.onCall(0).resolves([dataAssets[0]]);
+      stubModelGetById.onCall(1).resolves([dataAssets[0]]);
+      stubModelGetById.onCall(2).resolves([dataAssets[0]]);
+      stubModelGetById.onCall(3).resolves([dataAssets[1]]);
+      stubModelGetById.onCall(4).resolves([dataAssets[1]]);
+      stubModelGetById.onCall(5).resolves([dataAssets[1]]);
+      stubModelGetById.onCall(6).resolves([dataAssets[2]]);
+      stubModelGetById.onCall(7).resolves([dataAssets[2]]);
+      stubModelGetById.onCall(8).resolves([dataAssets[2]]);
+      stubModelGetById.onCall(9).resolves([]);
+      stubModelGetById.onCall(10).resolves([]);
+    });
+  
+    after(() => {
+      ativoModel.getByAssets.restore();
+    });
+  
+    it('Testa se o retorno para o id 1 é um array', async () => {
+      const response = await ativoService.getByAssets(1);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 1 não está vazio', async () => {
+      const response = await ativoService.getByAssets(1);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna as informações do ativo com id 1', async () => {
+      const [response] = await ativoService.getByAssets(1);
+      
+      expect(response).to.include.all.keys('CodAtivo', 'Valor', 'QtdeAtivo');
+    });
+
+    it('Testa se o retorno para o id 2 é um array', async () => {
+      const response = await ativoService.getByAssets(2);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 2 não está vazio', async () => {
+      const response = await ativoService.getByAssets(2);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna as informações do ativo com id 2', async () => {
+      const [response] = await ativoService.getByAssets(2);
+      
+      expect(response).to.include.all.keys('CodAtivo', 'Valor', 'QtdeAtivo');
+    });
+
+    it('Testa se o retorno para o id 3 é um array', async () => {
+      const response = await ativoService.getByAssets(3);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para o id 3 não está vazio', async () => {
+      const response = await ativoService.getByAssets(3);
+  
+      expect(response).to.not.be.empty;
+    });
+
+    it('Testa se retorna as informações do ativo com id 3', async () => {
+      const [response] = await ativoService.getByAssets(3);
+      
+      expect(response).to.include.all.keys('CodAtivo', 'Valor', 'QtdeAtivo');
+    });
+
+    it('Testa se o retorno para um id inválido é um array', async () => {
+      const response = await ativoService.getByAssets(4);
+  
+      expect(response).to.be.an('array');
+    });
+  
+    it('Testa se o array retornado para um id inválido está vazio', async () => {
+      const response = await ativoService.getByAssets(4);
+  
+      expect(response).to.be.empty;
+    });
+  });
+
 });

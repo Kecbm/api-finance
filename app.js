5c99c6d2baef3bdd018e1792aaab3a5f28bed714
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-20 18:18:05 -0300

    :sparkles: feat: middleware de erro

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const investimentosRoutes = require('./routers/investimentosRoutes');
 const ativoRoutes = require('./routers/ativoRoutes');
 const contaRoutes = require('./routers/contaRoutes');
+const error = require('./middlewares/error');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/investimentos', investimentosRoutes);
 app.use('/ativos', ativoRoutes);
 app.use('/conta', contaRoutes);
+app.use(error);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima

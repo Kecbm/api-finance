7c2a78332fd7362ec6303de57953c25199b23edb
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-21 09:21:58 -0300

    :hammer: refactor: validação apenas de valor

--- middlewares/validateConta.js
+++ middlewares/validateConta.js
@@ -1,21 +1,13 @@
-const validateBody= (req, res, next) => {
-  const { codCliente, valor } = req.body;
-
-  if (!codCliente) {
-    return res.status(400).json({ message: '"codCliente" é necessário' });
-  }
+const validateValor= (req, res, next) => {
+  const { valor } = req.body;
 
   if (!valor) {
     return res.status(400).json({ message: '"valor" é necessário' });
-  }
-
-  if (codCliente < 0) {
-    return res.status(422).json({ message: '"codCliente" deve ser maior que zero' });
   }
 
   next();
 };
 
 module.exports = {
-  validateBody,
+  validateValor,
 };
--- routers/contaRoutes.js
+++ routers/contaRoutes.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const contaController = require('../controllers/contaController');
-const { validateBody } = require('../middlewares/validateConta');
+const { validateValor } = require('../middlewares/validateConta');
+const { validateCodCliente } = require('../middlewares/validateInvestimentos');
 
-router.post('/deposito', validateBody, contaController.deposito);
-router.post('/saque', validateBody, contaController.saque);
+router.post('/deposito', validateValor, validateCodCliente, contaController.deposito);
+router.post('/saque', validateValor, validateCodCliente, contaController.saque);
 router.get('/:id', contaController.saldo);
 
 module.exports = router;

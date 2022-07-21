30d62db1e154460b519b42c7c62d6eb726498c4e
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-21 17:42:45 -0300

    :bug: fix: nome de ativo controller no singular

--- src/routers/ativoRoutes.js
+++ src/routers/ativoRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const ativoControllers = require('../controllers/ativoController');
-const { validateCodAtivo } = require('../middlewares/validateAtivo');
+const ativoController = require('../controllers/ativoController');
 
-router.get('/quantidade/:id', ativoControllers.quantity);
-router.get('/carteira/:id', validateCodAtivo, ativoControllers.carteira);
-router.get('/cliente/:id', ativoControllers.getByClient);
-router.get('/:id', ativoControllers.getByAssets);
+router.get('/quantidade/:id', ativoController.quantity);
+router.get('/cliente/:id', ativoController.getByClient);
+router.get('/:id', ativoController.getByAssets);
 
 module.exports = router;

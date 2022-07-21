71dadf70781e8c78d0abe6d998c02a1a683cbeb7
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-21 11:21:38 -0300

    :sparkles: feat: senhaCliente p/ JWT

--- database/XPInvestimentos.sql
+++ database/XPInvestimentos.sql
@@ -5,16 +5,17 @@ USE XPInvestimentos;
 CREATE TABLE cliente (
     codCliente INT NOT NULL auto_increment,
     nomeCliente VARCHAR(30) NOT NULL,
-    saldoDisponivel DECIMAL NOT NULL,
+    emailCliente VARCHAR(30) NOT NULL,
     senhaCliente VARCHAR(30) NOT NULL,
+    saldoDisponivel DECIMAL NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(codCliente)
 ) ENGINE=INNODB;
 
-INSERT INTO XPInvestimentos.cliente (nomeCliente, saldoDisponivel, senhaCliente, date) VALUES
-    ("Mariana Groppa", 1000, 'senha123', NOW()),
-    ("Ana K Melo", 1000, 'senha123', NOW()),
-    ("Clara Sodré", 1000, 'senha123', NOW());
+INSERT INTO XPInvestimentos.cliente (nomeCliente, emailCliente, senhaCliente, saldoDisponivel, date) VALUES
+    ("Mariana Groppa", "Mariana@email.com", "senha123", 1000, NOW()),
+    ("Ana K Melo", "Ana@email.com", "senha123", 1000, NOW()),
+    ("Clara Sodré", "Clara@email.com", "senha123", 1000, NOW());
 
 CREATE TABLE ativo (
     codAtivo INT NOT NULL auto_increment,

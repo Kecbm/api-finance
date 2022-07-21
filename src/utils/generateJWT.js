a8dc37c18bf39bbb5c463973e8a424bf5361a075
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-21 18:11:20 -0300

    :hammer: refactor: 10m p/ expirar token - seguro

--- src/utils/generateJWT.js
+++ src/utils/generateJWT.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const generateJWT = (user) => {
-  const jwtConfig = { expiresIn: '24h', algorithm: 'HS256' };
+  const jwtConfig = { expiresIn: '10m', algorithm: 'HS256' };
 
   const TOKEN_SECRET = process.env.JWT_SECRET;
 

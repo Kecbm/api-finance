581a7c2aad7115d7a72aa63a5dc0d1e107eef936
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-22 18:32:31 -0300

    :sparkles: feat: taxa de venda na corretora

--- src/utils/determinaTaxa.js
+++ src/utils/determinaTaxa.js
@@ -2,6 +2,15 @@ const taxaCompra = (qtdeAtivo, valorAtivo) => {
   const taxa = (qtdeAtivo * 0.02) * valorAtivo;
 
   return taxa;
+};
+
+const taxaVenda = (qtdeAtivo) => {
+  const taxa = (qtdeAtivo * 3.50);
+
+  return taxa;
 }
 
-module.exports = taxaCompra;
+module.exports = {
+  taxaCompra,
+  taxaVenda,
+};
